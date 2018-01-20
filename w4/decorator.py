a6745990f076e0f5fd7976c1b48e5429172acf33
dujia <greenpean@163.com>
2018-01-20 21:14:57 +0800

    chg

--- w4/decorator.py
+++ w4/decorator.py
@@ -6,16 +6,16 @@
 
 #装饰器
 
-def test1():
-    print("test1")
-    #time.sleep(3)
-
 def deco(func):
     print("add new func")
     return func
 
+@deco
+def test1():
+    print("test1")
+    #time.sleep(3)
 
 test1()
-print("------添加功能后-------")
-test1=deco(test1)
-test1()
+# print("------添加功能后-------")
+# test1=deco(test1)
+# test1()
